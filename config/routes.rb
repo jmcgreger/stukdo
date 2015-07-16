614ed660a818476bc43c5e3a147c5f4cee8de07a
James M. McGreger <james@wackykidgames.com>
2015-07-16 19:14:34 -0400

    Getting error on Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
   root 'pages#home'
 
   get 'about' => 'pages#about'
